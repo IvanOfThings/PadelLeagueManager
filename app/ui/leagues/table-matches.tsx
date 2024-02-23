547636efc4df176f72555f4ce128632ca2e5777d
Ivan Perea <ivan.perea@lansweeper.com>
2024-02-23 21:54:41 +0100

    :sparkles: not hidding in movile

--- app/ui/leagues/table-matches.tsx
+++ app/ui/leagues/table-matches.tsx
@@ -17,7 +17,7 @@ export default async function MatchesTable({
           <div className="py-2">
             <TableTitle title="Partidos"></TableTitle>
           </div>
-          <table className="hidden min-w-full text-gray-900 md:table">
+          <table className=" min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">

00234a42416b669edd87c3ca71cd7d1e8af37953
Ivan Perea <ivan.perea@lansweeper.com>
2024-02-24 23:16:52 +0100

    :art: improving titles on page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,9 +11,13 @@ import MyLeagues from '@/app/ui/dashboard/list-leagues';
 export default async function Page() {
   return (
     <main>
-      <h1 className={`mb-4 ml-10 text-2xl font-extrabold md:text-5xl`}>
-        Dashboard
-      </h1>
+      <div className="flex h-20 items-end justify-center rounded-md bg-blue-600 p-2 md:h-20">
+        <div className={`flex flex-row items-center leading-none text-white`}>
+          <h1 className={`mb-3 text-2xl font-extrabold md:text-5xl`}>
+            Dashboard
+          </h1>
+        </div>
+      </div>
 
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<LatestInvoicesSkeleton />}>
--- app/dashboard/leagues/[id]/page.tsx
+++ app/dashboard/leagues/[id]/page.tsx
@@ -15,9 +15,13 @@ export default async function Page({ params }: { params: { id: string } }) {
   const matches = await fetchMatches(params.id);
   return (
     <main>
-      <h1 className={`mb-4 ml-10 text-2xl font-extrabold md:text-5xl`}>
-        {`${league.name}`}{' '}
-      </h1>
+      <div className="flex h-20 items-end justify-center rounded-md bg-blue-600 p-2 md:h-20">
+        <div className={`flex flex-row items-center leading-none text-white`}>
+          <h1 className={`mb-3 text-2xl font-extrabold md:text-5xl`}>
+            {`${league.name}`}{' '}
+          </h1>
+        </div>
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <div className=" md:col-span-4 lg:col-span-6">
           <Suspense fallback={<LatestInvoicesSkeleton />}>
